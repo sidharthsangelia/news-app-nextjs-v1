0696938f4797e2746734db5b91530c606c3ad231
sidharthsangelia@gmail. <sidharthsangelia@gmail.com>
2025-04-06 20:54:40 +0530

    Initial commit with proper .gitignore

---
+++ app/archive/@latest/default.js
@@ -0,0 +1,14 @@
+import NewsList from "@/components/news-list";
+import { getLatestNews } from "@/lib/news";
+
+export default function LatestNewsPage() {
+
+     const latestNews = getLatestNews();
+    return (
+      <>
+        <h2>Latest News </h2>
+        <NewsList news={latestNews}></NewsList>
+      </>
+    );
+  }
+  
---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NewsNotFound() {
+  return (
+    <div id="error">
+      <h1>not-found</h1>
+      <p>Unfortunately, We could not find the requested article!!!</p>
+    </div>
+  );
+}
